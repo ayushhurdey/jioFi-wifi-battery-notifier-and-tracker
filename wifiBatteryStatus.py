66a8f4d4c3d3ba6ac84645214f3e6dd4d194eebf
ayush <ayushhurdey999@gmail.com>
2020-12-13 20:58:39 +0530

    notification now also shows data used

--- wifiBatteryStatus.py
+++ wifiBatteryStatus.py
@@ -7,6 +7,7 @@ from datetime import datetime,date
 import csv
 import os
 import re
+import math
 
 
 def if_file_exists_do_orElse(file_name):
@@ -29,6 +30,21 @@ def if_file_exists_do_orElse(file_name):
                                 time.sleep(300)
                                 if_file_exists_do_orElse(file_name)
 
+def getDataDetails():
+        data = {"Page":"GetWANInfo","mask":"0","token":"0"}
+        req = requests.post('http://jiofi.local.html/cgi-bin/qcmap_web_cgi', data).json()
+        download_data = req['total_data_used_dlink']
+        upload_data = req['total_data_used_ulink']
+        total_data = float(re.findall('([0-9]+.[0-9]+) MB',download_data)[0]) + float(re.findall('([0-9]+.[0-9]+) MB',upload_data)[0])
+        print('download data =',download_data, ', upload data=', upload_data, ', total data=', round(total_data, 2), 'MB\n')
+        return total_data
+
+def getClientsDetail():
+        data = {"Page":"GetLANInfo","mask":"0","token":"0"}
+        req = requests.post('http://jiofi.local.html/cgi-bin/qcmap_web_cgi', data).json()
+        no_of_clients = int(req['connected_client_count'])
+        print(no_of_clients)
+        return no_of_clients
 
 def get_battery():
         battery = str(bs4.BeautifulSoup(requests.get('http://jiofi.local.html/#').text,features="html.parser").select('#batterylevel')[0])
@@ -58,22 +74,27 @@ if __name__ == "__main__":
                         battery = get_battery()
                         battery_status = get_battery_status()
                 except:
-                        toast.show_toast("Wifi Not connected","Not on jioFi.",duration = 20,icon_path = r"\Users\admin\Pictures\wifi.ico")
+                        toast.show_toast("Wifi Not connected","Not on jioFi.",duration = 20,icon_path = r"assets\wifi.ico")
+                        time.sleep(200)
+                        continue
 
                 today = date.today()
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
                 print("Time =", current_time, " Wifi battery = ", str(battery) + "%  ", battery_status)
-
+                
                 try:
-                        print("writing data to file...\n")
+                        print("writing data to file...")
                         append_data_to_file(file_name, today.strftime("%b-%d-%Y"), current_time, str(battery) + "%", battery_status)
                 except PermissionError as e:
                         print("Problem opening file." + str(e))
-                
-                extremely_low_battery_msg = "Few minutes of battery left.\nBattery Left: " + str(battery) + "%" + "\nStatus: " + battery_status
-                medium_battery_msg = "Wifi battery is about to black out.\nBattery Left: " + str(battery) + "%" + "\nStatus: " + battery_status
-                battery_full_msg = "Battery Level: " + str(battery) + "%" + "\nStatus: " + battery_status
+
+                data_used = str(round(getDataDetails(), 2))
+                clients = getClientsDetail()
+                extremely_low_battery_msg = "Few minutes of battery left.\nBattery Left: " + str(battery) + "%" + "\nStatus: " + battery_status + "\nData used: " + data_used +" MB"
+                medium_battery_msg = "Wifi battery is about to black out.\nBattery Left: " + str(battery) + "%" + "\nStatus: " + battery_status + "\nData used: " + data_used + " MB"
+                battery_full_msg = "Battery Level: " + str(battery) + "%" + "\nStatus: " + battery_status + "\nData used: " + data_used + " MB"
+
                 
                 if(battery <= 10):
                         toast.show_toast("WARNING: Battery LOW",extremely_low_battery_msg,duration = 20,icon_path = r"assets\extremely_low_battery.ico")
